e0f9a69455b811b0102046e96965fa69f9f6d84c
Abhishek Kumar Rabidas <abhishek.rabidas@synlabs.io>
2023-06-16 15:33:51 +0530

    object data parsing presedence fix

--- UMRR/Communication/DataCommunication.py
+++ UMRR/Communication/DataCommunication.py
@@ -2,10 +2,10 @@ import struct
 
 from UMRR.Communication.MessageClasses import ObjectDetection, ObjectStatusMessage, StatusMessage
 
+OBJECT_DETECTION_BUFFER = []
+
 
 class DataCommunication:
-    def __init__(self):
-        self.object_detection_buffer = []
 
     def parse_status_message(self, payload):
         status_message = StatusMessage()
@@ -26,13 +26,15 @@ class DataCommunication:
         object_detection = ObjectDetection()
         object_detection.Time = timestamp
 
-        value = struct.unpack("<II", payload[0:8])[0]
-        xp1 = value & 0b00000000_00000000_00000000_00000000_00000000_00000000_00111111_11111110 >> 1
-        yp1 = value & 0b00000000_00000000_00000000_00000000_00000111_11111111_11000000_00000000 >> 14
-        sx1 = value & 0b00000000_00000000_00000000_00111111_11111000_00000000_00000000_00000000 >> 27
-        sy1 = value & 0b00000000_00000001_11111111_11000000_00000000_00000000_00000000_00000000 >> 38
-        oll = value & 0b00000000_11111110_00000000_00000000_00000000_00000000_00000000_00000000 >> 49
-        oid = value & 0b11111111_00000000_00000000_00000000_00000000_00000000_00000000_00000000 >> 56
+        value = int(payload[0]) | int(payload[1]) << 8 | int(payload[2]) << 16 | int(payload[3]) << 24 | int(
+            payload[4]) << 32 | int(payload[5]) << 40 | int(payload[6]) << 48 | int(payload[7]) << 56
+
+        xp1 = (value & 0b00000000_00000000_00000000_00000000_00000000_00000000_00111111_11111110) >> 1
+        yp1 = (value & 0b00000000_00000000_00000000_00000000_00000111_11111111_11000000_00000000) >> 14
+        sx1 = (value & 0b00000000_00000000_00000000_00111111_11111000_00000000_00000000_00000000) >> 27
+        sy1 = (value & 0b00000000_00000001_11111111_11000000_00000000_00000000_00000000_00000000) >> 38
+        oll = (value & 0b00000000_11111110_00000000_00000000_00000000_00000000_00000000_00000000) >> 49
+        oid = (value & 0b11111111_00000000_00000000_00000000_00000000_00000000_00000000_00000000) >> 56
 
         object_detection.ObjectID = int(oid)
         object_detection.X = (float(xp1) - 4096) * 0.128
@@ -42,10 +44,10 @@ class DataCommunication:
         object_detection.Length = float(oll) * 0.2
 
         # caching object detection output
-        if len(self.object_detection_buffer) > 10:
-            self.object_detection_buffer.clear()
+        if len(OBJECT_DETECTION_BUFFER) > 10:
+            OBJECT_DETECTION_BUFFER.clear()
         else:
-            self.object_detection_buffer.append(object_detection)
+            OBJECT_DETECTION_BUFFER.append(object_detection)
 
         return object_detection.__str__()
 
